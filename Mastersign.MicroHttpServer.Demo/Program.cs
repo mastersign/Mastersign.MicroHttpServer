b4556d0d41ee4861508f8f2ba396b7f4d6080fef
Tobias Kiertscher <dev@mastersign.de>
2021-01-14 18:04:19 +0000

    added demo project

---
+++ Mastersign.MicroHttpServer.Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Mastersign.MicroHttpServer.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var svr = new HttpServer();
+            svr.LogToConsole(LogLevel.Verbose);
+
+            svr.Use(new ExceptionHandler());
+            svr.Use(new TimingMiddleware(LogLevel.Information));
+            svr.Use(new CompressionMiddelware(new GZipCompressor(), new DeflateCompressor()));
+            svr.PathRouter()
+               .With("/", ctx => "Index")
+               .With("files", new FileHandler(@"F:\"))
+               .With("redirect", (ctx, _) => ctx.RedirectTemporarily("/about"))
+               .With("about", ctx => "About")
+               .PathRouter("api")
+                    .With("info", (ctx, _) => ctx.Respond(StringHttpResponse.Text("Info")))
+                    .Without(ctx => "API");
+            svr.Use(new NotFoundHandler());
+
+            svr.ListenToLoopback();
+            svr.Start();
+
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();
+        }
+    }
+}
