239c335ac97b30434dbd95087a3d47044c32d30d
Tobias Kiertscher <dev@mastersign.de>
2021-01-15 12:04:51 +0000

    added AnonymousByteArrayHttpRequestHandler

---
+++ Mastersign.MicroHttpServer/Handlers/AnonymousByteArrayHttpRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Mastersign.MicroHttpServer
+{
+    public class AnonymousByteArrayHttpRequestHandler : IHttpRequestHandler
+    {
+        private readonly Func<IHttpContext, byte[]> _method;
+        private readonly string _contentType;
+
+        public AnonymousByteArrayHttpRequestHandler(Func<IHttpContext, byte[]> method, string contentType = "application/octet-stream")
+        {
+            _method = method;
+            _contentType = contentType;
+        }
+
+        public Task Handle(IHttpContext context, Func<Task> next)
+        {
+            var result = _method(context);
+            if (result != null)
+            {
+                context.Response = ByteArrayHttpResponse.Create(result, 0, result.Length, contentType: _contentType);
+                return Task.Factory.GetCompleted();
+            }
+
+            return next();
+        }
+    }
+
+}
