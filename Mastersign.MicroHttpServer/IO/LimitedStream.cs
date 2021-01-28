576034ad3cabb9423dabc3d5bfcb7f24a2ab11ad
Tobias Kiertscher <tobias.kiertscher@th-brandenburg.de>
2021-01-28 18:59:27 +0000

    strip down LimitedStream again

--- Mastersign.MicroHttpServer/IO/LimitedStream.cs
+++ Mastersign.MicroHttpServer/IO/LimitedStream.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Mastersign.MicroHttpServer
 {
@@ -6,22 +7,21 @@ namespace Mastersign.MicroHttpServer
     {
         private const string _exceptionMessageFormat = "The stream has exceeded the {0} limit specified.";
         private readonly Stream _child;
-        private readonly long _offset;
         private long _readLimit;
         private long _writeLimit;
 
         public override bool CanRead => _child.CanRead;
 
-        public override bool CanSeek => _child.CanSeek;
+        public override bool CanSeek => false;
 
         public override bool CanWrite => _child.CanWrite;
 
-        public override long Length => _child.Length - _offset;
+        public override long Length => throw new NotSupportedException();
 
         public override long Position
         {
-            get => _child.Position - _offset;
-            set => _child.Position = value + _offset;
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
         }
 
         public override int ReadTimeout
@@ -39,7 +39,6 @@ namespace Mastersign.MicroHttpServer
         public LimitedStream(Stream child, long readLimit = -1, long writeLimit = -1)
         {
             _child = child;
-            _offset = child.CanSeek ? child.Position : 0L;
             _readLimit = readLimit;
             _writeLimit = writeLimit;
         }
@@ -51,33 +50,12 @@ namespace Mastersign.MicroHttpServer
 
         public override long Seek(long offset, SeekOrigin origin)
         {
-            long p = _child.Position;
-            long targetPosition;
-            switch (origin)
-            {
-                case SeekOrigin.Begin:
-                    targetPosition = _offset + offset;
-                    break;
-                case SeekOrigin.Current:
-                    targetPosition = p + offset;
-                    break;
-                case SeekOrigin.End:
-                    targetPosition = _child.Length - offset;
-                    break;
-                default:
-                    targetPosition = p;
-                    break;
-            }
-            if (targetPosition < _offset)
-            {
-                throw new IOException("Seeking position before the beginning of the stream.");
-            }
-            return _child.Seek(targetPosition, SeekOrigin.Begin);
+            throw new NotSupportedException();
         }
 
         public override void SetLength(long value)
         {
-            _child.SetLength(value + _offset);
+            throw new NotSupportedException();
         }
 
         public override int Read(byte[] buffer, int offset, int count)
