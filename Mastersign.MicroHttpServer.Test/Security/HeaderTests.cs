6c4ef303689ef457de9fac239531e5aba893765c
Tobias Kiertscher <tobias.kiertscher@th-brandenburg.de>
2021-01-28 19:37:52 +0000

    prevent over posting in the header

---
+++ Mastersign.MicroHttpServer.Test/Security/HeaderTests.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mastersign.MicroHttpServer.Test.Security
+{
+    public class HeaderTests
+    {
+        [Fact]
+        public async void DoAllowRequestLineEqualLimit()
+        {
+            var protocolOverhead = "GET  HTTP/1.1".Length;
+            var requestLineLimit = protocolOverhead + 200;
+            var requestProvider = new HttpRequestProvider { RequestLineLimit = requestLineLimit };
+            using (var server = new HttpServer(requestProvider: requestProvider))
+            {
+                server.ListenToLoopback(10001);
+                server.Use(ctx => EmptyHttpResponse.Create(HttpResponseCode.NoContent, keepAlive: false));
+                server.Start();
+
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    var request = new System.Net.Http.HttpRequestMessage(
+                        System.Net.Http.HttpMethod.Get, "http://127.0.0.1:10001" + "/" + new string('T', 200 - 1));
+                    var response = await client.SendAsync(request);
+                    Assert.Equal(System.Net.HttpStatusCode.NoContent, response.StatusCode);
+                }
+            }
+        }
+
+        [Fact]
+        public async void DoNotAllowRequestLineGreaterLimit()
+        {
+            var protocolOverhead = "GET  HTTP/1.1".Length;
+            var requestLineLimit = protocolOverhead + 200;
+            var requestProvider = new HttpRequestProvider { RequestLineLimit = requestLineLimit };
+            using (var server = new HttpServer(requestProvider: requestProvider))
+            {
+                server.ListenToLoopback(10002);
+                server.Use(ctx => EmptyHttpResponse.Create(HttpResponseCode.NoContent, keepAlive: false));
+                server.Start();
+
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    var request = new System.Net.Http.HttpRequestMessage(
+                        System.Net.Http.HttpMethod.Get, "http://127.0.0.1:10002" + "/" + new string('T', 200 - 1 + 1));
+                    await Assert.ThrowsAsync<System.Net.Http.HttpRequestException>(async () =>
+                    {
+                        await client.SendAsync(request);
+                    });
+                }
+            }
+        }
+
+        [Fact]
+        public async void DoAllowSingleHeaderLineEqualLimit()
+        {
+            const int LIMIT = 200;
+            var requestProvider = new HttpRequestProvider { HeaderLineLimit = LIMIT };
+            using (var server = new HttpServer(requestProvider: requestProvider))
+            {
+                server.ListenToLoopback(10003);
+                server.Use(ctx => EmptyHttpResponse.Create(HttpResponseCode.NoContent, keepAlive: false));
+                server.Start();
+
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    var request = new System.Net.Http.HttpRequestMessage(
+                        System.Net.Http.HttpMethod.Get, "http://127.0.0.1:10003");
+                    request.Headers.Add("X-Test", new string('T', LIMIT - 8));
+                    var response = await client.SendAsync(request);
+                    Assert.Equal(System.Net.HttpStatusCode.NoContent, response.StatusCode);
+                }
+            }
+        }
+
+        [Fact]
+        public async void DoNotAllowSingleHeaderLineGreaterLimit()
+        {
+            const int LIMIT = 200;
+            var requestProvider = new HttpRequestProvider { HeaderLineLimit = LIMIT };
+            using (var server = new HttpServer(requestProvider: requestProvider))
+            {
+                server.ListenToLoopback(10004);
+                server.Use(ctx => EmptyHttpResponse.Create(HttpResponseCode.NoContent, keepAlive: false));
+                server.Start();
+
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    var request = new System.Net.Http.HttpRequestMessage(
+                        System.Net.Http.HttpMethod.Get, "http://127.0.0.1:10004");
+                    request.Headers.Add("X-Test", new string('T', LIMIT - 8 + 1));
+                    await Assert.ThrowsAsync<System.Net.Http.HttpRequestException>(async () =>
+                    {
+                        await client.SendAsync(request);
+                    });
+                }
+            }
+        }
+
+        [Fact]
+        public async void DoAllowTotalHeaderEqualLimit()
+        {
+            int protocolLineLength = "GET / HTTP/1.1\r\n".Length;
+            int hostHeaderLength = "Host: 127.0.0.1:10005\r\n".Length;
+            int keepAliveHeaderLength = "Connection: Keep-Alive\r\n".Length;
+            int totalLimit = protocolLineLength + hostHeaderLength + keepAliveHeaderLength + 200;
+            var requestProvider = new HttpRequestProvider { TotalHeaderLimit = totalLimit };
+            using (var server = new HttpServer(requestProvider: requestProvider))
+            {
+                server.ListenToLoopback(10005);
+                server.Use(ctx => EmptyHttpResponse.Create(HttpResponseCode.NoContent, keepAlive: false));
+                server.Start();
+
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    client.DefaultRequestHeaders.Clear();
+                    var request = new System.Net.Http.HttpRequestMessage(
+                        System.Net.Http.HttpMethod.Get, "http://127.0.0.1:10005");
+                    // is required for compatibility, because net461 always sets it by it self
+                    request.Headers.Add("Connection", "Keep-Alive");
+                    for (var i = 0; i < 10; i++)
+                    {
+                        // 20 chars per header line
+                        //     8 chars for "X-T-00: "
+                        //     10 chars for the value
+                        //     2 chars for \r\n
+                        request.Headers.Add($"X-T-{i:00}", new string('T', 10));
+                    }
+                    
+                    var response = await client.SendAsync(request);
+                    Assert.Equal(System.Net.HttpStatusCode.NoContent, response.StatusCode);
+                }
+            }
+        }
+
+        [Fact]
+        public async void DoNotAllowTotalHeaderGreaterLimit()
+        {
+            int protocolLineLength = "GET / HTTP/1.1\r\n".Length;
+            int hostHeaderLength = "Host: 127.0.0.1:10006\r\n".Length;
+            int keepAliveHeaderLength = "Connection: Keep-Alive\r\n".Length;
+            int totalLimit = protocolLineLength + hostHeaderLength + keepAliveHeaderLength + 200 - 1;
+            var requestProvider = new HttpRequestProvider { TotalHeaderLimit = totalLimit };
+            using (var server = new HttpServer(requestProvider: requestProvider))
+            {
+                server.ListenToLoopback(10006);
+                server.Use(ctx => EmptyHttpResponse.Create(HttpResponseCode.NoContent, keepAlive: false));
+                server.Start();
+
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    client.DefaultRequestHeaders.Clear();
+                    var request = new System.Net.Http.HttpRequestMessage(
+                        System.Net.Http.HttpMethod.Get, "http://127.0.0.1:10006");
+                    // is required for compatibility, because net461 always sets it by it self
+                    request.Headers.Add("Connection", "Keep-Alive");
+                    for (var i = 0; i < 10; i++)
+                    {
+                        // 20 chars per header line
+                        //     8 chars for "X-T-00: "
+                        //     10 chars for the value
+                        //     2 chars for \r\n
+                        request.Headers.Add($"X-T-{i:00}", new string('T', 10));
+                    }
+                    await Assert.ThrowsAsync<System.Net.Http.HttpRequestException>(async () =>
+                    {
+                        await client.SendAsync(request);
+                    });
+                }
+            }
+        }
+    }
+}
--- Mastersign.MicroHttpServer/HttpRequests/HttpRequestProvider.cs
+++ Mastersign.MicroHttpServer/HttpRequests/HttpRequestProvider.cs
@@ -8,59 +8,136 @@ namespace Mastersign.MicroHttpServer
 {
     public class HttpRequestProvider : IHttpRequestProvider
     {
-        private static readonly Stream _emptyStream = new EmptyStream();
+        public int TotalHeaderLimit { get; set; } = 1024 * 4;
+
+        public int RequestLineLimit { get; set; } = 1024;
+
+        public int HeaderLineLimit { get; set; } = 1024;
+
+        public bool AllowUTF8Header { get; set; }
 
         public async Task<IHttpRequest> Provide(Stream stream)
         {
-            // parse the http request
+            var streamReader = new StreamReader(stream, AllowUTF8Header ? Encoding.UTF8 : Encoding.ASCII);
+            var consumed = 0;
 
-            // use UTF-8 decoding, dispite the fact that only ASCII characters are allowed in header fields,
-            // some clients send UTF-8 sequences anyway
-            var streamReader = new StreamReader(stream, Encoding.UTF8);
-            var request = await streamReader.ReadLineAsync().ConfigureAwait(false);
+            ParsedRequestLine requestLine = ParsedRequestLine.Failed;
+            IStringLookup headers = null;
 
-            if (request == null) return null;
+            await Task.Run(() =>
+            {
+                requestLine = ParseRequestLine(streamReader, ref consumed);
+                if (!requestLine.Success) return;
+
+                headers = ParseHeaderLines(streamReader, ref consumed);
+            });
+            if (!requestLine.Success || headers == null) return null;
+
+            IHttpRequest request = new HttpRequest(requestLine.Method, requestLine.Uri, requestLine.Protocol, headers, stream);
+            return request;
+        }
+
+        private ParsedRequestLine ParseRequestLine(StreamReader r, ref int consumed)
+        {
+            var request = ReadLimitedLine(r, RequestLineLimit);
+
+            if (request == null) return ParsedRequestLine.Failed;
+            consumed += request.Length + 2;
 
             var firstSpace = request.IndexOf(' ');
             var lastSpace = request.LastIndexOf(' ');
+
+            if (lastSpace <= firstSpace) return ParsedRequestLine.Failed;
 
             var tokens = new[] {
                 request.Substring(0, firstSpace),
                 request.Substring(firstSpace + 1, lastSpace - firstSpace - 1),
                 request.Substring(lastSpace + 1)
             };
+            if (tokens.Length != 3) return ParsedRequestLine.Failed;
 
-            if (tokens.Length != 3) return null;
+            var method = HttpMethodProvider.Default.Provide(tokens[0]);
+            var uriString = tokens[1];
+            var uri = uriString.StartsWith("http://") || uriString.StartsWith("https://")
+                ? new Uri(uriString, UriKind.Absolute)
+                : new Uri("http://0.0.0.0" + uriString, UriKind.Absolute);
+            var protocol = tokens[2];
 
-            var httpMethod = HttpMethodProvider.Default.Provide(tokens[0]);
-            var httpProtocol = tokens[2];
-
-            var uri = new Uri("http://server" + tokens[1], UriKind.Absolute);
-
-            // get the headers
-            var headersRaw = new List<KeyValuePair<string, string>>();
-            string line;
-            while (!string.IsNullOrEmpty(line = await streamReader.ReadLineAsync().ConfigureAwait(false)))
-            {
-                var currentLine = line;
-                var headerKvp = SplitHeader(currentLine);
-                if (headerKvp.HasValue) headersRaw.Add(headerKvp.Value);
-            }
-            var headers = headersRaw.ToStringLookup();
-
-            var contentStream = headers.TryGetByName("Content-Length", out long length)
-                ? new LimitedStream(stream, length, length)
-                : _emptyStream;
-
-            return new HttpRequest(httpMethod, uri, httpProtocol, headers, contentStream);
+            return new ParsedRequestLine(method, uri, protocol);
         }
 
-        private KeyValuePair<string, string>? SplitHeader(string header)
+        private IStringLookup ParseHeaderLines(StreamReader r, ref int consumed)
+        {
+            var headersRaw = new List<KeyValuePair<string, string>>();
+            string line;
+            while (!string.IsNullOrEmpty(line = ReadLimitedLine(r, HeaderLineLimit)))
+            {
+                consumed += line.Length + 2;
+                if (consumed > TotalHeaderLimit)
+                {
+                    return null;
+                }
+                var headerKvp = SplitHeader(line);
+                if (headerKvp.HasValue) headersRaw.Add(headerKvp.Value);
+            }
+            if (line == null) return null;
+            return headersRaw.ToStringLookup();
+        }
+
+        private static string ReadLimitedLine(StreamReader r, int limit)
+        {
+            var sb = new StringBuilder();
+            var exceededLimit = false;
+            int c;
+            var hadCR = false;
+            while ((c = r.Read()) >= 0)
+            {
+                if (sb.Length > limit)
+                {
+                    exceededLimit = true;
+                    break;
+                }
+                if (c == '\r')
+                {
+                    hadCR = true;
+                }
+                else if (hadCR && c == '\n')
+                {
+                    break;
+                }
+                else
+                {
+                    hadCR = false;
+                    sb.Append((char)c);
+                }
+            }
+            return exceededLimit ? null : sb.ToString();
+        }
+
+        private static KeyValuePair<string, string>? SplitHeader(string header)
         {
             var index = header.IndexOf(": ", StringComparison.Ordinal);
             return index > 0
                 ? new KeyValuePair<string, string>(header.Substring(0, index), header.Substring(index + 2))
                 : (KeyValuePair<string, string>?)null;
+        }
+
+        private struct ParsedRequestLine
+        {
+            public static ParsedRequestLine Failed = new ParsedRequestLine { Success = false };
+
+            public bool Success;
+            public HttpMethod Method;
+            public Uri Uri;
+            public string Protocol;
+
+            public ParsedRequestLine(HttpMethod method, Uri uri, string protocol)
+            {
+                Success = true;
+                Method = method;
+                Uri = uri;
+                Protocol = protocol;
+            }
         }
     }
 }
