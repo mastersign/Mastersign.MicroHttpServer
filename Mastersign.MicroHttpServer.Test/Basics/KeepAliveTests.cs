1ad704a814cbdd9275ee4427fbb6293928136dc1
Tobias Kiertscher <dev@mastersign.de>
2024-07-02 13:02:54 +0200

    added keep-alive test

---
+++ Mastersign.MicroHttpServer.Test/Basics/KeepAliveTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mastersign.MicroHttpServer.Test.Basics
+{
+    public class KeepAliveTests
+    {
+        [Fact]
+        public async void ConnectionStaysOpenWithKeepAlive()
+        {
+            using (var server = new HttpServer())
+            {
+                server.ListenToLoopback(10201);
+                server.GetAll("content");
+                server.Start();
+
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    var request = new System.Net.Http.HttpRequestMessage(
+                        System.Net.Http.HttpMethod.Get, "http://127.0.0.1:10201/");
+
+                    var response = await client.SendAsync(request);
+                    Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+                    var content = await response.Content.ReadAsStringAsync();
+                    Assert.Equal("content", content);
+                }
+            }
+        }
+    }
+}
