0c62005d2168481aa0e65975727bb1d857663708
Tobias Kiertscher <dev@mastersign.de>
2024-09-13 12:39:23 +0200

    implemented POST tests -> failing on .NET >= 5

--- Mastersign.MicroHttpServer.Test/Basics/SimpleRequestTests.cs
+++ Mastersign.MicroHttpServer.Test/Basics/SimpleRequestTests.cs
@@ -60,31 +60,36 @@ namespace Mastersign.MicroHttpServer.Test.Basics
         }
 
         [Fact]
-        public async void PostText()
+        public async void PostBytesReadIndividually()
         {
-            var text = "Line ①\nLĩne 2\n";
-            var encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
-            var textBytes = encoding.GetBytes(text);
-            var expectedContentLength = textBytes.LongLength;
+            var payload = new byte[23 * 77 * 123]; // some random length, not fitting into a power of two buffer scheme
+            var rand = new Random();
+            rand.NextBytes(payload);
+
+            var expectedContentLength = payload.LongLength;
+            var expectedContentType = "application/x-test-bytes";
+
             long actualContentLength = -1L;
+            string actualContentType = null;
             long beginStreamPosition = -1L;
             long endStreamPosition = -1L;
-            byte[] receivedTextBytes = null;
+            byte[] receivedPayload = null;
             var readBytes = 0L;
 
             using (var server = new HttpServer())
             {
-                server.ListenToLoopback(10103);
+                server.ListenToLoopback(10111);
                 server.PostAll(ctx =>
                 {
                     actualContentLength = ctx.Request.ContentLength();
-                    receivedTextBytes = new byte[actualContentLength];
+                    actualContentType = ctx.Request.ContentType();
+                    receivedPayload = new byte[actualContentLength];
                     beginStreamPosition = ctx.Request.ContentStream.Position;
                     for (var i = 0L; i <= actualContentLength; i++)
                     {
                         var b = ctx.Request.ContentStream.ReadByte();
                         if (b < 0) break;
-                        receivedTextBytes[i] = (byte)(b & 0xFF);
+                        receivedPayload[i] = (byte)(b & 0xFF);
                         readBytes++;
                     }
                     endStreamPosition = ctx.Request.ContentStream.Position;
@@ -92,19 +97,140 @@ namespace Mastersign.MicroHttpServer.Test.Basics
                 });
                 server.Start();
 
-                using (var client = new System.Net.Http.HttpClient() { Timeout = TimeSpan.FromSeconds(1) })
+                using (var client = new System.Net.Http.HttpClient() { Timeout = TimeSpan.FromSeconds(2) })
                 {
                     var request = new System.Net.Http.HttpRequestMessage(
-                        System.Net.Http.HttpMethod.Post, "http://127.0.0.1:10103/");
-                    request.Content = new System.Net.Http.StringContent(text, encoding, "text/plain");
+                        System.Net.Http.HttpMethod.Post, "http://127.0.0.1:10111/");
+                    request.Content = new System.Net.Http.ByteArrayContent(payload);
+                    request.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(expectedContentType);
 
                     var response = await client.SendAsync(request);
                     Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
 
                     Assert.Equal(expectedContentLength, actualContentLength);
+                    Assert.Equal(expectedContentType, actualContentType);
                     Assert.Equal(0L, beginStreamPosition);
                     Assert.Equal(expectedContentLength, endStreamPosition);
-                    Assert.Equal(textBytes, receivedTextBytes);
+                    Assert.Equal(payload, receivedPayload);
+
+                    var content = await response.Content.ReadAsStringAsync();
+                    Assert.Equal("OK", content);
+                }
+            }
+        }
+
+        [Fact]
+        public async void PostBytesReadSync()
+        {
+            //var payload = new byte[23 * 77 * 123]; // some random length, not fitting into a power of two buffer scheme
+            var payload = new byte[512]; // some random length, not fitting into a power of two buffer scheme
+            var rand = new Random();
+            rand.NextBytes(payload);
+
+            var expectedContentType = "application/x-test-bytes";
+
+            byte[] receivedPayload = null;
+            string actualContentType = null;
+
+            using (var server = new HttpServer())
+            {
+                server.ListenToLoopback(10112);
+                server.PostAll(ctx =>
+                {
+                    actualContentType = ctx.Request.ContentType();
+                    var contentLength = ctx.Request.ContentLength();
+                    receivedPayload = ctx.Request.ReadContentAsBytes();
+                    return "OK";
+                });
+                server.Start();
+
+                using (var client = new System.Net.Http.HttpClient() { Timeout = TimeSpan.FromSeconds(2) })
+                {
+                    var request = new System.Net.Http.HttpRequestMessage(
+                        System.Net.Http.HttpMethod.Post, "http://127.0.0.1:10112/");
+                    request.Content = new System.Net.Http.ByteArrayContent(payload);
+                    request.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(expectedContentType);
+
+                    var response = await client.SendAsync(request);
+                    Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+                    Assert.Equal(expectedContentType, actualContentType);
+                    Assert.Equal(payload, receivedPayload);
+
+                    var content = await response.Content.ReadAsStringAsync();
+                    Assert.Equal("OK", content);
+                }
+            }
+        }
+
+        [Fact]
+        public async void PostBytesReadAsync()
+        {
+            var payload = new byte[23 * 77 * 123]; // some random length, not fitting into a power of two buffer scheme
+            var rand = new Random();
+            rand.NextBytes(payload);
+
+            var expectedContentType = "application/x-test-bytes";
+
+            byte[] receivedPayload = null;
+            string actualContentType = null;
+
+            using (var server = new HttpServer())
+            {
+                server.ListenToLoopback(10113);
+                server.PostAll(async ctx =>
+                {
+                    actualContentType = ctx.Request.ContentType();
+                    receivedPayload = await ctx.Request.ReadContentAsBytesAsync();
+                    return "OK";
+                });
+                server.Start();
+
+                using (var client = new System.Net.Http.HttpClient() { Timeout = TimeSpan.FromSeconds(2) })
+                {
+                    var request = new System.Net.Http.HttpRequestMessage(
+                        System.Net.Http.HttpMethod.Post, "http://127.0.0.1:10113/");
+                    request.Content = new System.Net.Http.ByteArrayContent(payload);
+                    request.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(expectedContentType);
+
+                    var response = await client.SendAsync(request);
+                    Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+                    Assert.Equal(expectedContentType, actualContentType);
+                    Assert.Equal(payload, receivedPayload);
+
+                    var content = await response.Content.ReadAsStringAsync();
+                    Assert.Equal("OK", content);
+                }
+            }
+        }
+
+        [Fact]
+        public async void PostText()
+        {
+            var text = "Line ①\nLĩne 2\n";
+            var receivedText = string.Empty;
+
+            using (var server = new HttpServer())
+            {
+                server.ListenToLoopback(10114);
+                server.PostAll(ctx =>
+                {
+                    receivedText = ctx.Request.ReadContentAsString();
+                    return "OK";
+                });
+                server.Start();
+
+                using (var client = new System.Net.Http.HttpClient() { Timeout = TimeSpan.FromSeconds(2) })
+                {
+                    var request = new System.Net.Http.HttpRequestMessage(
+                        System.Net.Http.HttpMethod.Post, "http://127.0.0.1:10114/");
+                    request.Content = new System.Net.Http.StringContent(text, Encoding.UTF8, "text/plain");
+
+                    var response = await client.SendAsync(request);
+                    Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+                    Assert.Equal(text, receivedText);
 
                     var content = await response.Content.ReadAsStringAsync();
                     Assert.Equal("OK", content);
